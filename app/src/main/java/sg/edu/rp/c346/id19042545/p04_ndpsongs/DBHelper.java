0a47777a039219fce42f2f1bfdc240aa2ecda88f
daryl13579 <19042545@myrp.edu.sg>
2021-05-17 12:10:29 +0800

    Init

---
+++ app/src/main/java/sg/edu/rp/c346/id19042545/p04_ndpsongs/DBHelper.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c346.id19042545.p04_ndpsongs;
+
+public class DBHelper {
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id19042545/p04_ndpsongs/Song.java
@@ -0,0 +1,52 @@
+package sg.edu.rp.c346.id19042545.p04_ndpsongs;
+
+public class Song {
+    private int _id;
+    private String title;
+    private String singers;
+    private int years;
+    private int stars;
+
+    public Song(String title, String singers, int years, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.years = years;
+        this.stars = stars;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSingers(String singers) {
+        this.singers = singers;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+}
